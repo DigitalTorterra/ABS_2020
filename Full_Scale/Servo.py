27adde71ea649667705f22b150e63ea6086ed347
Patrick <pfaley@nd.edu>
2020-02-13 23:01:46 -0500

    Servo again

--- Full_Scale/Servo.py
+++ Full_Scale/Servo.py
@@ -1,5 +1,8 @@
-import RPI.GPIO as GPIO
+import pigpio
 from time import sleep
+import os
+
+os.system('sudo pigpiod')
 
 class Servo():
     def __init__(self):
@@ -8,26 +11,24 @@ class Servo():
         self.max_phi = 67.5
 
         #2100/2500 (2500==1)
-        self.min_dc = .6  
-        self.max_dc = .84
+        self.min_dc = int(.6*255)
+        self.max_dc = int(.84)
         
-        GPIO.setmode(GPIO.BOARD)
+        self.pin = 12
+        self.freq = 400
 
         #Set up PWM pin
-        GPIO.setup(32,GPIO.OUT)
-        self.p = GPIO.PWM(32,400) #pin, freq
-        self.p.start(self.min_dc)
+        self.pi = pigpio.pi()
+        self.pi.set_PWM_frequency(self.pin,self.freq)
 
     def rotate(self,phi):
         #write some conversion between phi and duty cycle
         if phi >= self.min_phi and phi <= self.max_phi:
             norm_phi = (phi-self.min_phi)/(self.max_phi-self.min_phi)
             new_cycle = self.min_dc + norm_phi*(self.max_dc-self.min_dc)
-        
-            self.p.ChangeDutyCycle(new_cycle)
+            new_cycle = int(new_cycle)
+
+            self.pi.set_PWM_dutycycle(self.pin,new_cycle)
         else:
             print('Invalid rotation given')
 
-    def __del__(self):
-        self.p.stop()
-        GPIO.cleanup()
